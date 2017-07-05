a8a4f7e29b5fa2b38eeadd87b5a3daec1f998cfc
heimish96 <heimish.w@gmail.com>
2017-07-06 02:06:24 +0900

    Modify sum modules

--- sum.c
+++ sum.c
@@ -3,12 +3,12 @@
 int sum(int n)
 {
 	int res=0;
-	for(int i=0; i<=n; i++)
-	{
-		res+=i;
-		return res;
-	}
 
+	for(int i = 1; i <= n; i++)
+	
+		res+=i;
+	
+	return res;
 }
 
 
--- sum.h
+++ sum.h
@@ -1,6 +1,9 @@
 #ifndef __SUM_H__
 #define __SUM_H__
+
 int sum(int n);
+
 #endif
+
 
 
